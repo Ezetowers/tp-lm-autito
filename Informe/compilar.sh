35bd460c4b6fba998a2c3aced1bc6078ad5c12b6
ezequiel.torresfeyuk <ezequiel.torresfeyuk@gmail.com>
2012-06-21 02:36:06 +0000

    Agrego el cuerpo del Informe

---
+++ Informe/compilar.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+nombre="Informe"
+# Compilo dos veces para que aparezca el índice
+pdflatex $nombre.tex
+pdflatex $nombre.tex
+
+# Borro los archivos auxiliares
+rm $nombre.toc $nombre.aux $nombre.log $nombre.out 
